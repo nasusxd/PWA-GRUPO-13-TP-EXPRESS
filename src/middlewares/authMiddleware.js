444149cd5003323684f4eab27a8861fc68380302
nasusxd <js8918657@gmail.com>
2026-06-20 20:55:55 -0300

    hice las validaicones, termine endpoints y agregue una dependencia

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,12 +1,10 @@
-import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
-
 
 export const auth = (roles = []) => {
     return (req, res, next) => {
         const authHeader = req.headers.authorization
 
-        if (!authHeader?.startsWith('Bearer ')) {
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
             return res.status(401).json({ message: 'No autorizado' })
         }
 
@@ -17,11 +15,11 @@ export const auth = (roles = []) => {
             req.user = tokenInfo
 
             if (roles.length > 0 && !roles.includes(tokenInfo.rol)) {
-                    return res.status(403).json({ message: 'Prohibido: Permisos insuficientes' })
-                }
-                next()
+                return res.status(403).json({ message: 'Prohibido: Permisos insuficientes' })
+            }
+            next()
         } catch (error) {
-            return res.status(401).json({message: 'Token expirado o no valido'})
+            return res.status(401).json({ message: 'Token expirado o no valido' })
         }
     }
 }
